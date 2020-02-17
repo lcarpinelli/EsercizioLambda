2076148c0eb4c22f54a3629c781fc3635d301015
l.carpinelli <l.carpinelli@VETRYA-PC100>
2020-02-17 17:19:58 +0100

    Aggiunta Lambda getMinimum

--- EsercizioLambda/Program.cs
+++ EsercizioLambda/Program.cs
@@ -13,24 +13,27 @@ namespace EsercizioLambda
             //Esercizio 2
             Func<int, int, int, int> getMaximum = (first, second, third) => { int max = first; if(second > max) {max = second;} if(third > max) {max = third;} return max; };
             //Esercizio 3
-            Action<DateTime, DateTime> printLowerDate = (data1, data2) => { if (data1 > data2) {Console.WriteLine($"Piu grande {data2.ToString("dd/MM/yyyy")}"); } else { Console.WriteLine($"Piu grande {data1.ToString("dd/MM/yyyy")}"); }; };
+            Action<DateTime, DateTime> printLowerDate = (data1, data2) => { if (data1 > data2) {Console.WriteLine($"Tra {data1.ToString("dd/MM/yyyy")} e {data2.ToString("dd/MM/yyyy")} la data piu grande è {data2.ToString("dd/MM/yyyy")}"); } else{ Console.WriteLine($"Tra {data1.ToString("dd/MM/yyyy")} e {data2.ToString("dd/MM/yyyy")} la data piu grande è {data1.ToString("dd/MM/yyyy")}"); };};
+            //Esercizio 4
+            Func<int,int,int,int> getMinimum = (first, second, third) => { int min = first; if (second < min) { min = second; } if (third < min) { min = third; } return min; };
 
-            //Esercizio 1
-            int first = 1;
-            int second = 2;
-            int third = 4;
+            //Esercizio 1 e Esercizio 4
+            int first = 51;
+            int second = 62;
+            int third = 84;
             int max = getMaximum(first, second, third);
-            Console.WriteLine(max);
+            int min = getMinimum(first, second, third);
+            Console.WriteLine($"Il valore più grande tra {first}-{second}-{third} è {max} e il valore più piccolo è {min}");
 
             //Esercizio 2
-            string name = "NOME";
-            string surname = "COGNOME";
+            string name = "Lorenzo";
+            string surname = "Carpinelli";
             string result = concatFirstAndLastName(name, surname);
-            Console.WriteLine(result);
+            Console.WriteLine($"Il suo nome completo è {result}");
 
             //Esercizio 3 e Esempio
             DateTime date1 = new DateTime(2000, 12, 20);
-            DateTime date2 = new DateTime(2000, 8, 23);
+            DateTime date2 = new DateTime(2002, 12, 20);
             printLowerDate(date1, date2);
             canDrive(date1);
             canDrive(date2);
